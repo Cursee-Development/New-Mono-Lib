944935606d10557f18d577ef0ab51377c88232ba
jason13official <jason13official@gmail.com>
2024-10-19 15:32:07 -0400

    refactor

---
+++ fabric/src/main/java/com/cursee/monolib/MonoLibFabric.java
@@ -0,0 +1,13 @@
+package com.cursee.monolib;
+
+import com.cursee.monolib.core.sailing.Sailing;
+import net.fabricmc.api.ModInitializer;
+
+public class MonoLibFabric implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+        MonoLib.init();
+        Sailing.register(Constants.MOD_NAME, Constants.MOD_ID, Constants.MOD_VERSION, Constants.MC_VERSION_RAW, Constants.PUBLISHER_AUTHOR, Constants.PRIMARY_CURSEFORGE_MODRINTH);
+    }
+}
---
+++ neoforge/src/main/java/com/cursee/monolib/MonoLibNeoForge.java
@@ -0,0 +1,14 @@
+package com.cursee.monolib;
+
+import com.cursee.monolib.core.sailing.Sailing;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class MonoLibNeoForge {
+    
+    public MonoLibNeoForge(IEventBus modEventBus) {
+        MonoLib.init();
+        Sailing.register(Constants.MOD_NAME, Constants.MOD_ID, Constants.MOD_VERSION, Constants.MC_VERSION_RAW, Constants.PUBLISHER_AUTHOR, Constants.PRIMARY_CURSEFORGE_MODRINTH);
+    }
+}
