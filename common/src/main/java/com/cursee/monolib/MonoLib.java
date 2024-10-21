a9b84aef494ac77cb69670378bd93fc4d0b7b044
jason13official <jason13official@gmail.com>
2024-10-20 20:23:20 -0400

    initialize configuration

--- common/src/main/java/com/cursee/monolib/MonoLib.java
+++ common/src/main/java/com/cursee/monolib/MonoLib.java
@@ -1,6 +1,10 @@
 package com.cursee.monolib;
 
+import com.cursee.monolib.core.MonoLibConfiguration;
+
 public class MonoLib {
 
-    public static void init() {}
+    public static void init() {
+        MonoLibConfiguration.initialize();
+    }
 }
