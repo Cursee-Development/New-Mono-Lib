1d818b9f5d57b6ada0fbd235944a19fc24320a58
jason13official <jason13official@gmail.com>
2024-10-20 21:26:57 -0400

    fix versions for 1.19.2

--- common/src/main/java/com/cursee/monolib/Constants.java
+++ common/src/main/java/com/cursee/monolib/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	public static final String MOD_ID = "monolib";
 	public static final String MOD_NAME = "MonoLib";
 	public static final String MOD_VERSION = "1.4.0";
-	public static final String MC_VERSION_RAW = "[1.20.1]";
+	public static final String MC_VERSION_RAW = "[1.19.2]";
 	public static final Pair<String, String> PUBLISHER_AUTHOR = new Pair<String, String>("Lupin", "Jason13");
 	public static final Triplet<String, String, String> PRIMARY_CURSEFORGE_MODRINTH = new Triplet<String, String, String>("https://www.curseforge.com/members/lupin/projects", "https://www.curseforge.com/members/lupin/projects", "https://modrinth.com/user/Lupin/mods");
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
